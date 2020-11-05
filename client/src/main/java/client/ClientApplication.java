ad3c4b6e990a756e3bff12a537ca3d57daa4c940
wangwenjie <13716689854@163.com>
2020-11-05 15:55:45 +0800

    ** springboot 获取生效的配置文件

--- client/src/main/java/client/ClientApplication.java
+++ client/src/main/java/client/ClientApplication.java
@@ -2,8 +2,12 @@ package client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 /**
  *
@@ -14,7 +18,15 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ClientApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String[] activeProfiles = environment.getActiveProfiles();
+        if (activeProfiles.length > 0) {
+            String activeProfile = activeProfiles[0];
+            System.out.println("==> 当前生效的配置文件 = " + activeProfile);
+        }
+        Map<String, Object> systemProperties = environment.getSystemProperties();
+        System.out.println(systemProperties);
     }
 
     @Bean
